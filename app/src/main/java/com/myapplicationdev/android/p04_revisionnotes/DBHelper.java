27be63474bc70382f0317d661d2d21bec9790cf0
nickngweipeng <15056201@myrp.edu.sg>
2017-05-11 18:58:15 +0800

    Finalized

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
@@ -12,9 +12,12 @@ import java.util.ArrayList;
 public class DBHelper extends SQLiteOpenHelper {
 
 	//TODO Define the Database properties
-	private static final String DATABASE_NAME = "";
-	private static final int DATABASE_VERSION = ;
-
+	private static final String DATABASE_NAME = "notes.db";
+	private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NOTE = "note";
+	private static final String COLUMN_ID = "_id";
+	private static final String COLUMN_NOTECONTENT = "noteContent";
+	private static final String COLUMN_STARS = "stars";
 
 	public DBHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -22,7 +25,12 @@ public class DBHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		//TODO CREATE TABLE Note
+		String createTableSql = "CREATE TABLE " + TABLE_NOTE + "("
+				+ COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+				+ COLUMN_NOTECONTENT + " TEXT,"
+				+ COLUMN_STARS + " INTEGER)";
+		db.execSQL(createTableSql);
+		Log.i("info", "created tables");
 
 	}
 
@@ -34,10 +42,38 @@ public class DBHelper extends SQLiteOpenHelper {
 
 	public void insertNote(String noteContent, int stars) {
 		//TODO insert the data into the database
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		values.put(COLUMN_NOTECONTENT, noteContent);
+		values.put(COLUMN_STARS, stars);
+		db.insert(TABLE_NOTE, null, values);
+		db.close();
 	}
 
 	public ArrayList<Note> getAllNotes() {
 		//TODO return records in Java objects
+		ArrayList<Note> notes = new ArrayList<Note>();
+		String selectQuery = "SELECT " + COLUMN_ID + ", "
+				+ COLUMN_NOTECONTENT + ", "
+                + COLUMN_STARS
+				+ "FROM " + TABLE_NOTE;
+
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor cursor = db.rawQuery(selectQuery, null);
+
+		if(cursor.moveToFirst()){
+			do {
+				int id = cursor.getInt(0);
+				String notecontent = cursor.getString(1);
+				int star = cursor.getInt(2);
+				Note obj = new Note(id, notecontent, star);
+				notes.add(obj);
+			} while(cursor.moveToNext());
+		}
+		cursor.close();
+		db.close();
+
+		return notes;
 	}
 
     public ArrayList<String> getNoteContent() {
@@ -46,7 +82,7 @@ public class DBHelper extends SQLiteOpenHelper {
 		// Create an ArrayList that holds String objects
         ArrayList<String> notes = new ArrayList<String>();
         // Select all the notes' content
-        String selectQuery = "";
+        String selectQuery = "SELECT " + COLUMN_NOTECONTENT + " FROM " + TABLE_NOTE;
 
         // Get the instance of database to read
         SQLiteDatabase db = this.getReadableDatabase();
@@ -56,8 +92,8 @@ public class DBHelper extends SQLiteOpenHelper {
         if (cursor.moveToFirst()) {
             // Loop while moveToNext() points to next row and returns true;
             // moveToNext() returns false when no more next row to move to
+            notes.add(cursor.getString(0));
             do {
-
 
             } while (cursor.moveToNext());
         }
--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
@@ -1,13 +1,51 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert, btnShow;
+    RadioGroup rg;
+    EditText etNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInsert = (Button)findViewById(R.id.buttonInsertNote);
+        btnShow = (Button)findViewById(R.id.buttonShowList);
+        etNote = (EditText)findViewById(R.id.editTextNote);
+        rg = (RadioGroup)findViewById(R.id.radioGroupStars);
+        int selectedButtonNum = rg.getCheckedRadioButtonId();
+        final RadioButton rb = (RadioButton)findViewById(selectedButtonNum);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedRb = Integer.parseInt(rb.getText().toString());
+                DBHelper db = new DBHelper(MainActivity.this);
+                String content = etNote.getText().toString();
+                db.insertNote(content, selectedRb);
+                db.close();
+                Toast.makeText(MainActivity.this, "Added Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
